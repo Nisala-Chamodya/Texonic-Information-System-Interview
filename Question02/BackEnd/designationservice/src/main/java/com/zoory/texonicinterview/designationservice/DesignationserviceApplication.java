27b7ab49d515513b21b63d283b457050ac11e4e7
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-03-16 13:29:23 +0530

    Initilaizng backend

---
+++ Question02/BackEnd/designationservice/src/main/java/com/zoory/texonicinterview/designationservice/DesignationserviceApplication.java
@@ -0,0 +1,14 @@
+package com.zoory.texonicinterview.designationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignationserviceApplication.class, args);
+		System.out.println("start");
+	}
+
+}
---
+++ Question02/BackEnd/designationservice/src/test/java/com/zoory/texonicinterview/designationservice/DesignationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoory.texonicinterview.designationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
